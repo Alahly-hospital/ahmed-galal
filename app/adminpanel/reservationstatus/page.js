e5c2fd553c04a01e88149c7107e026def6cedb03
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2023-09-03 20:12:33 +0300

    admin panel page modifing

--- app/adminpanel/reservationstatus/page.js
+++ app/adminpanel/reservationstatus/page.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import f from'../../../assets/ff.jpg'
-import m from'../../../assets/mm.jpg'
+import React from "react";
+import f from "../../../assets/ff.jpg";
+import m from "../../../assets/mm.jpg";
 
 export default function reservationstatus() {
   return (
-    <>   <div className="row shadow rounded-5">
-    <div className="col-lg-12  rounded d-flex">
-      <div className="col-lg-2">
-                    <img src={m.src}  className="w-100 rounded-circle border-2"/>
+    <>
+       <div className="row  rounded-5">
+        <div className="col-lg-5 shadow rounded-5 ">
+          <div className=" text-center">
+            <img src={m.src} className="w-50 rounded-circle" />
+          </div>
 
-      </div>
-      
-      <div className="col-lg-3 mt-4 pt-4">
-        <h5> الاسم</h5>
-        <h5> الهاتف</h5>
-      
-       </div>
+          <div className="">
+            <div className="d-flex  justify-content-around">
+              <h5>
+                 موعد الحجز &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </h5>
 
-<div className="col-lg-3  mt-4 pt-4">
-      
-        <h5> البريد الالكتروني</h5>
-        <h5> موعد الحجز </h5>
-       
-       </div>
-       <div className="col-lg-3  mt-4 pt-4">
+              <h5> الهاتف &nbsp;&nbsp;</h5>
+            </div>
+            <div className="d-flex justify-content-around">
+              <h5> البريد الالكتروني</h5> <h5> الاسم &nbsp;&nbsp;&nbsp;</h5>
+            </div>
+
+            <div className="d-flex justify-content-around">
+              <h5> تفاصيل عن الحجز</h5>
+
+              <h5> النوع &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h5>
+            </div>
+
+            <div className="  d-flex justify-content-center mb-4 ">
+              <button className="btn primary-sidebar-bg ms-2  text-white">
+                متاح
+              </button>
+              <button className="btn bg-danger ms-2  text-white ">
+                غير متاح
+              </button>
+            </div>
+          </div>
  
-      <h5>   النوع</h5>
-      <h5>   اي تفاصيل عن الحجز</h5>
-     </div>            
-
-
-    </div>
-    <div className="col-lg-12  d-flex justify-content-center mb-3">
-<button className="btn primary-sidebar-bg ms-2  text-white">متاح</button>
-<button className="btn bg-danger ms-2  text-white ">غير متاح</button>
-</div>
-
-  </div></>
-  )
+        </div>
+      </div>
+    </>
+  );
 }

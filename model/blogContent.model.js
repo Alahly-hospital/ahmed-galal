2fcd834ad487b82ee489ad1bc53ad5cad1a21b8a
Rawan-abdelfattah <rwanabdelfattah301@gmail.com>
2024-02-16 17:14:05 +0200

    commit

--- model/blogContent.model.js
+++ model/blogContent.model.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const blogsSchema = new Schema({
-  blog: { type: mongoose.Types.ObjectId, required: true, ref: "blog" },
+  blog: { type: mongoose.Types.ObjectId, required: true, ref: "blog" },         
   title: { type: String, trim: true },
   content: { type: String },
   image: { type: String, trim: true },
   video: { type: String, trim: true },
+  subtitle: { type: String, trim: true },
   type: { type: String, required: true, trim: true },
   list: [{ type: String, trim: true }],
 });
--- routes/blogContent.route.js
+++ routes/blogContent.route.js
@@ -3,8 +3,8 @@ const blogsController = require("../controllers/blogContent..control")
 const auth = require("../middlewares/auth")
 const {blogPhotoUpload} = require("../middlewares/imageUpload")
 
-router.route("/")
-    .post(auth.authentication,blogPhotoUpload.single("image"),blogsController.createBlog)
+router.route("/")         
+    .post(blogPhotoUpload.single("image"),blogsController.createBlog)
     .patch(auth.adminAuthorization,blogsController.updateBlog)
     
     router.route("/:id")
